9c14335869db50c9a783f11bf3e6779015a2bdd3
Jackson Hammond <jbhammon@users.noreply.github.com>
2019-07-09 21:21:22 -0400

    registered initial models with admin page

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from plans.models import User, Plan, Course, AssignmentType, Assignment, DaysOff
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Plan)
+admin.site.register(Course)
+admin.site.register(AssignmentType)
+admin.site.register(Assignment)
+admin.site.register(DaysOff)
